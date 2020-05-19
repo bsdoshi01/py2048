d99edfe4d57f73403f9526a844e20b7b89df2704
bsdoshi01 <bsdoshi01@gmail.com>
2020-05-19 10:38:03 +0530

    All moves made are working, yet to add logic to break an invalid move and screen refresh left

--- 2048.py
+++ 2048.py
@@ -43,68 +43,118 @@ def add_zero_after_move(game_board, game_size, list_of_zeros_i, list_of_zeros_j)
 	game_board[list_of_zeros_i[location_of_random_zero]][list_of_zeros_j[location_of_random_zero]] = 2
 	return game_board
 
-'''
-def making_a_move(game_board, game_size, usr_input):
+def check_valid_move(game_board, game_size, usr_input):
+	flag = True
 	if usr_input == 'w':
-		for j in range(game_size):
+		game_board_new = move_by_usr_up(game_board, game_size)
+		if game_board_new == game_board:
 			flag = False
-			while not flag:
-				count = 0
-				for i in range(game_size):
-					if game_board[i][j] == 0:
-						count += 1
-				if count == game_size:
-					flag = True
-				if count == game_size-1:
-					for i in range(game_size):
-						if game_board[i][j]:
-							game_board[0][j], game_board[i][j] = game_board[i][j], 0
-	return game_board
-'''
-'''
-def making_a_move(game_board, game_size, usr_input):
-	if usr_input == 'w':
-		for j in range(game_size):
-			i, last_zero = 0, 0
-			while i < game_size:
-				if game_board[i][j] != 0:
-					if game_board[i-1][j] == 0 and i > 0:
-						game_board[last_zero][j], game_board[i][j] = game_board[i][j], 0
-					else:
-						k = i + 1
-						while k < game_size:
-							if game_board[k][j] != 0:
-								if game_board[k][j] == game_board[i][j]:
-									game_board[i][j] *= 2
-									game_board[k][j] = 0
-								k = game_size
-							else:
-								k += 1
-				else:
-					last_zero = i
-				i += 1
-	return game_board
-'''
+	elif usr_input == 's':
+		game_board_new = move_by_usr_down(game_board, game_size)
+		if game_board_new == game_board:
+			flag = False
+	elif usr_input == 'a':
+		game_board_new = move_by_usr_left(game_board, game_size)
+		if game_board_new == game_board:
+			flag = False
+	elif usr_input == 'd':
+		game_board_new = move_by_usr_right(game_board, game_size)
+		if game_board_new == game_board:
+			flag = False
+	else:
+		flag = False
+	return game_board, flag
 
-def making_a_move(game_board, game_size, usr_input):
-	if usr_input == 'w':
-		for j in range(game_size):
-			i = 0
-			while i < game_size:
-				if game_board[i][j] != 0 and i != 0:
-					k = i - 1
-					while k >= 0:
-						if game_board[k][j] != 0:
-							if game_board[k][j] == game_board[i][j]:
-								game_board[k][j] *= 2
-								game_board[i][j], k, i = 0, 0, k + 1
-							elif game_board[k][j] != game_board[i][j] and k != 0:
+def move_by_usr_up(game_board, game_size):
+	for j in range(game_size):
+		i = 0
+		while i < game_size:
+			if game_board[i][j] != 0 and i != 0:
+				k = i - 1
+				while k >= 0:
+					if game_board[k][j] != 0:
+						if game_board[k][j] == game_board[i][j]:
+							game_board[k][j] *= 2
+							game_board[i][j], k, i = 0, 0, k + 1
+						elif game_board[k][j] != game_board[i][j]:
+							if k + 1 == i:
+								k, i = 0, k + 1
+							else:
 								game_board[k+1][j], game_board[i][j] = game_board[i][j], 0
-								k, i = 0, k + 2
-						if k == 0 and game_board[k][j] == 0:
-							game_board[k][j], game_board[i][j], i = game_board[i][j], 0, k + 1
-						k -= 1
-				i += 1
+								k, i = 0, k + 1
+					if k == 0 and game_board[k][j] == 0:
+						game_board[k][j], game_board[i][j], i = game_board[i][j], 0, k + 1
+					k -= 1
+			i += 1
+	return game_board
+
+def move_by_usr_down(game_board, game_size):
+	for j in range(game_size):
+		i = game_size - 1
+		while i >= 0:
+			if game_board[i][j] != 0 and i != game_size:
+				k = i + 1
+				while k < game_size:
+					if game_board[k][j] != 0:
+						if game_board[k][j] == game_board[i][j]:
+							game_board[k][j] *= 2
+							game_board[i][j], k, i = 0, game_size, k - 1
+						elif game_board[k][j] != game_board[i][j]:
+							if k - 1 == i:
+								k, i = game_size, k - 1
+							else:
+								game_board[k-1][j], game_board[i][j] = game_board[i][j], 0
+								k, i = game_size, k - 1
+					if k == game_size - 1 and game_board[k][j] == 0:
+						game_board[k][j], game_board[i][j], i = game_board[i][j], 0, k - 1
+					k += 1
+			i -= 1
+	return game_board
+
+def move_by_usr_left(game_board, game_size):
+	for i in range(game_size):
+		j = 0
+		while j < game_size:
+			if game_board[i][j] != 0 and j != 0:
+				k = j - 1
+				while k >= 0:
+					if game_board[i][k] != 0:
+						if game_board[i][k] == game_board[i][j]:
+							game_board[i][k] *= 2
+							game_board[i][j], k, j = 0, 0, k + 1
+						elif game_board[j][k] != game_board[i][j]:
+							if k + 1 == j:
+								k, j = 0, k + 1
+							else:
+								game_board[i][k+1], game_board[i][j] = game_board[i][j], 0
+								k, j = 0, k + 1
+					if k == 0 and game_board[i][k] == 0:
+						game_board[i][k], game_board[i][j], j = game_board[i][j], 0, k + 1
+					k -= 1
+			j += 1
+	return game_board
+
+def move_by_usr_right(game_board, game_size):
+	for i in range(game_size):
+		j = game_size - 1
+		while j >= 0:
+			if game_board[i][j] != 0 and j != game_size:
+				k = j + 1
+				while k < game_size:
+					if game_board[i][k] != 0:
+						if game_board[i][k] == game_board[i][j]:
+							game_board[i][k] *= 2
+							game_board[i][j], k, j = 0, game_size, k - 1
+						elif game_board[i][k] != game_board[i][j]:
+							if k - 1 == j:
+								k, j = game_size, k - 1
+							else:
+								game_board[i][k-1], game_board[i][j] = game_board[i][j], 0
+								k, j = game_size, k - 1
+					if k == game_size - 1 and game_board[i][k] == 0:
+						game_board[i][k], game_board[i][j], j = game_board[i][j], 0, k - 1
+					k += 1
+			j -= 1
 	return game_board
 
 ''' LOGIC:
@@ -134,7 +184,7 @@ if __name__ == "__main__":
 	game_over = False
 	while not game_over:
 		usr_input = get_user_input()
-		game_board = making_a_move(game_board, game_size, usr_input)
+		game_board, flag = check_valid_move(game_board, game_size, usr_input)
 		list_of_zeros_i, list_of_zeros_j = check_zeros(game_board, game_size)
 		add_zero_after_move(game_board, game_size, list_of_zeros_i, list_of_zeros_j)	
 		print_board(game_board, game_size)
